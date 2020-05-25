e56e52d3f5d8d0bbad51630f49a3936b5a92e2e9
Lorraine <Lorraine@Isaacs-MacBook-Pro.local>
2020-05-25 13:18:48 -0500

    favorites

--- app/components/Carousel.js
+++ app/components/Carousel.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { Image } from "react-native-elements";
+import { ActivityIndicator } from "react-native";
 import Carousel from "react-native-snap-carousel";
 
 export default function CarouselImages(props) {
   const { arrayImages, height, width } = props;
 
   const renderItem = ({ item }) => {
-    return <Image style={{ width, height }} source={{ uri: item }} />;
-  };
+     return <Image 
+                style={{ width, height }} 
+                source={{ uri: item }}
+                PlaceholderContent={<ActivityIndicator color="#fff" />} 
+            />;
+  }
 
   return (
     <Carousel
-        layout={'default'}
+        layout={'tinder'}
         data={arrayImages}
         sliderWidth={width}
         itemWidth={width}
